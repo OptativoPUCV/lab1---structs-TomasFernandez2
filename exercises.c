e95f52d15e40af6aa65ab5f627509541107d34db
Your Name <you@example.com>
2024-03-14 21:12:20 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -29,7 +29,10 @@ Descripción: Escribe una función que tome un arreglo y su tamaño, y luego
 invierta el orden de sus elementos.
 */
 void reverseArray(int arr[], int size) 
-{}
+{
+  int temp;
+  
+}
 
 /*
 Ejercicio 3: Filtrar Números Pares
@@ -94,7 +97,22 @@ Descripción: Escribe una función que tome un arreglo y su tamaño,
 y luego devuelva 1 si el arreglo está ordenado en orden ascendente,
   0 si no está ordenado, y -1 si está ordenado en orden descendente.
 */
-int checkSorted(int arr[], int size) { return -2; }
+int checkSorted(int arr[], int size) 
+{
+  for (int i = 0; i < size; i++)
+    {
+      if (arr[i] < arr[i+1])
+      {
+        return 1;
+      }
+      if (arr[i] > arr[i+1])
+      {
+        return -1;
+      }
+    }
+  return 0; 
+}
+
 
 /*
 Ejercicio 6: Información de una Biblioteca
