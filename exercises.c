92a44e72dc0ea7447e0772ed63d463bbdcfdc5f5
Your Name <you@example.com>
2024-03-18 01:41:41 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -83,29 +83,33 @@ void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,
 {
   int i = 0, j = 0, k = 0
   while (i < size1 && j < size2)
-    {
-      if (arr1[i] < arr2[j])
-      {
-        result[k] = arr1[i];
-        i++;
-      }
-      else
-      {
-        result[k] = arr2[j];
-        j++;
-      }
-      k++;
-    }
-  while (i < size1)
+  {
+    if (arr1[i] < arr2[j])
     {
       result[k] = arr1[i];
+      i++;
     }
-  while (j < size2)
+    else
     {
       result[k] = arr2[j];
+      j++;
     }
+    k++;
+  }
+  while (i < size1)
+  {
+    result[k] = arr1[i];
+    i++;
+    k++;
+  }
+  while (j < size2)
+  {
+    result[k] = arr2[j];
+    j++;
+    k++;
+  }
 }
-
+    
 /*
 Ejercicio 5: Comprobación de Ordenación
 Descripción: Escribe una función que tome un arreglo y su tamaño,
