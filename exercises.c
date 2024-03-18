8f4c98a5fa2acbd33a64713aa3c44a5c240ee4ed
Your Name <you@example.com>
2024-03-18 01:49:48 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -120,12 +120,12 @@ int checkSorted(int arr[], int size)
 {
   for(int i = 0;i < size-1;i++)
   {
-      if(arr[i] >= arr[i+1])
+      if (arr[i] >= arr[i+1])
       {
         return 0;
        
       }
-      else
+      if(arr[i] <= arr[i+1]))
       {
         return 1;
       }
